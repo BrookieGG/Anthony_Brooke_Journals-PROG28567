8c4187b446f05cef43b50a698e2616e67f2e4ea2
BrookieGG <brooke4444@me.com>
2025-09-11 19:16:01 -0400

    Task 1 B - Bomb Trails

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -6,6 +6,11 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public List<Transform> asteroidTransforms;
+
+    public float bombTrailSpacing = 10;
+    public int numberOfTrailBombs = 5;
+    //private float inBombSpacing = 0;
+    //private int inNumberOfBombs;
     
     // Update is called once per frame
     void Update()
@@ -14,10 +19,24 @@ public class Player : MonoBehaviour
         {
             SpawnBombAtOffset(new Vector3(0, 1));
         }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            SpawnBombTrail(bombTrailSpacing, numberOfTrailBombs);
+        }
     }
     private void SpawnBombAtOffset(Vector3 inOffset) //Spawn Bomb at Offset
     {
         Vector3 spawnPosition = transform.position + inOffset;
         Instantiate(bombPrefab, spawnPosition, Quaternion.identity);
+    }
+
+    private void SpawnBombTrail(float inBombSpacing, int inNumberOfBombs)
+    {
+        for (int i = 1; i <= inNumberOfBombs; i++)
+        {
+            Vector3 spawnPos = transform.position - transform.up * (inBombSpacing * i);
+            Instantiate(bombPrefab, spawnPos, Quaternion.identity);
+        }
     }
 }
