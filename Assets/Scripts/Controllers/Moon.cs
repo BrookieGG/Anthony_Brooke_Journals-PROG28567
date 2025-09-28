51a97c7d855b62b819163595144858fe19e51063
BrookieGG <brooke4444@me.com>
2025-09-28 19:23:52 -0400

    Task 3 - orbiting moon

--- Assets/Scripts/Controllers/Moon.cs
+++ Assets/Scripts/Controllers/Moon.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Moon : MonoBehaviour
 {
     public Transform planetTransform;
+    private float angle = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,15 @@ public class Moon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        OrbitalMotion(5,20,planetTransform);
+    }
+    private void OrbitalMotion(float radius, float speed, Transform target)
+    {
+        angle = angle + speed * Time.deltaTime;
+        float ang = Mathf.Deg2Rad * angle;
+        transform.position = new Vector3(Mathf.Cos(ang), Mathf.Sin(ang), 0) * radius + planetTransform.position;
+        Debug.Log("angle: " + ang);
+        
 
     }
 }
